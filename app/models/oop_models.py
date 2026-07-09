dabaf53bdf89d00d606084f184492b15677cb772
Amaan <amanabbassaiyed@gmail.com>
2026-07-09 08:32:31 +0530

    OOPS practice Inheritance, dunder methods

--- app/models/oop_models.py
+++ app/models/oop_models.py
@@ -5,6 +5,9 @@ class BaseEntity:
     def __init__(self):
         pass
 
+    def papaMethod(self):
+        return "This is a method from the parent class"
+
 class Hostel(BaseEntity):
     id_counter = 1
     def __init__(self, name, cordinates):
@@ -27,6 +30,9 @@ class User(BaseEntity):
     def getUserInfo(self):
         return f"User: {self.name}, Hostel: {self.hostel_id}, Room: {self.room}, Role: {self.role}"
     
+    def __repr__(self):
+        return f"User(id={self.id}, name='{self.name}', email='{self.email}', room='{self.room}', hostel_id={self.hostel_id}, role='{self.role}')"
+    
     @property
     def email(self):
         return self._email
@@ -36,9 +42,20 @@ class User(BaseEntity):
         if "@" not in value:
             raise ValueError("Invalid email address")
         self._email = value
+    
+    def __eq__(self,other):
+        if not isinstance(other,User):
+            return f"Cannot compare User with {type(other)}"
+        return self.hostel_id == other.hostel_id and self.room == other.room
+
+    def papaMethod(self):
+        print(super().papaMethod())
+        return f"And this is a method from the child class"
 
 amaan = User("Amaan", "amaan@example.com", "Room 101", 1, "student")
+abbas = User("Abbas", "abbas@example.com", "Room 102", 2, "student")
 
 print(amaan.email)
-
+print(amaan==abbas)
+print(amaan.papaMethod())
 
